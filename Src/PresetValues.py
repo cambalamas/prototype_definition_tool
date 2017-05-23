5180d4b807f46c711d18db0604e2252123133833
cambalamas <cambalamas@gmail.com>
2017-05-23 16:23:59 +0200

    preset values modifies

--- Src/PresetValues.py
+++ Src/PresetValues.py
@@ -14,15 +14,14 @@ pv = {
 	'endMsg' 			: 'SESSION ENDED !\n\n',
 
 	'defaultPath' 		: '~',
-	'bgColor'			: '#DDD',
-	'sceneColor'		: '#DDD',
+	'bgColor'			: '#EEE',
 
-	'moveTimer'			: 2.0, #secs
-	'resizeTimer'		: 5.0, #secs
+	'moveTimer'			: 2.0, 		#secs
+	'resizeTimer'		: 5.0, 		#secs
 
 	'zJump'				: 1.0,
 	'maxOpacity'		: 1.0,
-	'noVisibleOpacity'	: 0.000,	# Invisible pero interactuable (0,001)
+	'noVisibleOpacity'	: 0.000,	# Invisible pero interactuable (0.001)
 	'noActiveOpacity'	: 0.25,
 
 	'imgModScale'		: 1.025,
@@ -30,7 +29,7 @@ pv = {
 	'imgMaxScale'		: 200.00,
 
 	'viewModScale'		: 1.1,
-	'viewMinScale'		: 0.05, 	# Como photoshop se hace muy pequeño
+	'viewMinScale'		: 0.05, 	# Como photoshop = muy pequeño (0.0013)
 	'viewMaxScale'		: 34.5,		# Como photoshop
 	'viewRectMargin'	: 0.70,
 
